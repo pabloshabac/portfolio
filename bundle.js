0f383cd1d316e56358c9e8574a12d789285b36c8
pabloshabac <nikolicgoran80@gmail.com>
2016-11-21 22:52:09 +0100

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -212,7 +212,7 @@ function portfolioComponent () {
       yo.update(element, template())
     }
  		function openNewTab() {
-    var url = "https://djape48.github.io/kviz"
+    var url = "https://pabloshabac.github.io/kviz"
     var win = window.open(url, '_blank')
     win.focus()
   }
